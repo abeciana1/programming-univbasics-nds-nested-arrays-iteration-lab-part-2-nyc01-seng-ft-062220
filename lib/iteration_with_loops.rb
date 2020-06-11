9761061191b832a4cc900e9644e7e408b3658583
Alex Beciana <alex.beciana@gmail.com>
2020-06-11 21:20:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
     inner_index = 0
     mini =  200
     while inner_index < src[outer_index].length do
-      if mini > src[outer_index][inner_index]
+      if src[outer_index][inner_index] < mini
         mini = src[outer_index][inner_index]
       
       end
